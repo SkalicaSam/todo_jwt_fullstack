7021391cc1540ddbe1938203d8f166e43a41369f
SkalicaSam <samcostehlik@gmail.com>
2024-09-09 00:35:54 +0200

    created first page in react

---
+++ frontend_reactJs/todolist/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { Router } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <> 
+    
+     <div>
+      <h1>Vítejte na mé jednoduché stránce v Reactu! <br></br>
+         http://localhost:3000/</h1>
+      <p>Toto je moje první stránka vytvořená v ReactJS.</p>
+    </div>
+    
+        
+    </>
+  );
+}
+
+export default App;
+
+
+
